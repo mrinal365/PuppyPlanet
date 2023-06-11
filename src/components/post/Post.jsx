a9ffdb7918ba3cc6d9eea639d56b018eb217a5fd
Mrinal <mrinaltewary@gmail.com>
2023-06-11 14:38:17 +0530

    post+feed initial

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function Post(){
+    return(
+        <div className='post'>
+            <div className="postWrapper">
+                <div className="postTop">
+                    <div className="postTopLeft">
+                        <img src="/assets/lab1.jpg" alt="" className="postProfileImage" />
+                        <span className='postUsername'>Mrinal</span>
+                        <span className="postDate">5 Min ago</span>
+                    </div>
+                    <div className="postTopRight">
+                        
+                    </div>
+                </div>
+                <div className="postCenter">
+
+                </div>
+                <div className="postBottom">
+
+                </div>
+            </div>
+            
+            Post
+        </div>
+    )
+}
