05fdc7a576ebc43cb5eeeb8bc8f930a47adc78b9
Mrinal <mrinaltewary@gmail.com>
2023-06-16 23:56:29 +0530

    post structure

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,28 +1,38 @@
 import React from 'react';
+import { MoreVert } from '@material-ui/icons';
 
+
+import './post.css'
 export default function Post(){
     return(
         <div className='post'>
             <div className="postWrapper">
                 <div className="postTop">
                     <div className="postTopLeft">
-                        <img src="/assets/lab1.jpg" alt="" className="postProfileImage" />
-                        <span className='postUsername'>Mrinal</span>
+                        <img src="/assets/lab1.jpg" alt="" className="postProfilePicture   " />
+                        <span className="postUsername">Mrinal</span>
                         <span className="postDate">5 Min ago</span>
                     </div>
                     <div className="postTopRight">
-                        
+                        <MoreVert/>
                     </div>
                 </div>
                 <div className="postCenter">
-
+                    <span className="postText">Hey! Its my First Post</span>
+                    <img className='postImg' src="assets/lab1.jpg" alt=""/>
                 </div>
                 <div className="postBottom">
-
+                    <div className="postBottomLeft">
+                        <img src="" alt="" className="likeIcon" />
+                        <img src="" alt="" className="likeIcon" />
+                        <span className="likeCounter">32 people liked it</span>
+                    </div>
+                    <div className="postBottomRight">
+                        <span className="postCommentText">9 Comments</span>
+                    </div>
                 </div>
             </div>
             
-            Post
         </div>
     )
 }
